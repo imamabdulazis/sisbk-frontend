8037d24d7af9f8e53c4a706b7614f2ebbb753b75
Imam <imam.azis@pgi-data.com>
2021-10-05 14:27:20 +0700

    add register campus logo

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,40 +1,47 @@
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink } from "react-router-dom";
 // material
-import { styled } from '@material-ui/core/styles';
-import { Box, Card, Link, Container, Typography } from '@material-ui/core';
+import { styled } from "@material-ui/core/styles";
+import { Box, Card, Link, Container, Typography, Stack } from "@material-ui/core";
 // layouts
-import AuthLayout from '../layouts/AuthLayout';
+import AuthLayout from "../layouts/AuthLayout";
 // components
-import Page from '../components/Page';
-import { MHidden } from '../components/@material-extend';
-import { RegisterForm } from '../components/authentication/register';
-import AuthSocial from '../components/authentication/AuthSocial';
+import Page from "../components/Page";
+import { MHidden } from "../components/@material-extend";
+import { RegisterForm } from "../components/authentication/register";
+import AuthSocial from "../components/authentication/AuthSocial";
 
 // ----------------------------------------------------------------------
 
 const RootStyle = styled(Page)(({ theme }) => ({
-  [theme.breakpoints.up('md')]: {
-    display: 'flex'
-  }
+  [theme.breakpoints.up("md")]: {
+    display: "flex",
+  },
 }));
 
 const SectionStyle = styled(Card)(({ theme }) => ({
-  width: '100%',
+  width: "100%",
   maxWidth: 464,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  margin: theme.spacing(2, 0, 2, 2)
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  margin: theme.spacing(2, 0, 2, 2),
 }));
 
-const ContentStyle = styled('div')(({ theme }) => ({
+const ContentStyle = styled("div")(({ theme }) => ({
   maxWidth: 480,
-  margin: 'auto',
-  display: 'flex',
-  minHeight: '100vh',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  padding: theme.spacing(12, 0)
+  margin: "auto",
+  display: "flex",
+  minHeight: "100vh",
+  flexDirection: "column",
+  justifyContent: "center",
+  padding: theme.spacing(12, 0),
+}));
+
+const LogoStyle = styled("div")(({ theme }) => ({
+  width: 100,
+  height: 100,
+  display: "flex",
+  alignSelf: "center",
 }));
 
 // ----------------------------------------------------------------------
@@ -44,7 +51,12 @@ export default function Register() {
     <RootStyle title="Register">
       <AuthLayout>
         Sudah punya akun? &nbsp;
-        <Link underline="none" variant="subtitle2" component={RouterLink} to="/login">
+        <Link
+          underline="none"
+          variant="subtitle2"
+          component={RouterLink}
+          to="/login"
+        >
           Login
         </Link>
       </AuthLayout>
@@ -54,7 +66,16 @@ export default function Register() {
           <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
             Mulai kelola potensi dirimu
           </Typography>
-          <img alt="register" src="/static/illustrations/illustration_register.png" />
+          <img
+            alt="register"
+            src="/static/illustrations/illustration_register.png"
+          />
+          <Stack direction="row">
+            <LogoStyle>
+              <img src="/static/logo/uty.png" alt="uty" />
+              <img src="/static/logo/smk.jpeg" alt="smk" />
+            </LogoStyle>
+          </Stack>
         </SectionStyle>
       </MHidden>
 
@@ -64,7 +85,7 @@ export default function Register() {
             <Typography variant="h4" gutterBottom>
               Mulai daftar yuk.
             </Typography>
-            <Typography sx={{ color: 'text.secondary' }}>
+            <Typography sx={{ color: "text.secondary" }}>
               Pastikan data yang di isi benar.
             </Typography>
           </Box>
@@ -73,20 +94,24 @@ export default function Register() {
 
           <RegisterForm />
 
-          <Typography variant="body2" align="center" sx={{ color: 'text.secondary', mt: 3 }}>
+          <Typography
+            variant="body2"
+            align="center"
+            sx={{ color: "text.secondary", mt: 3 }}
+          >
             By registering, I agree to Minimal&nbsp;
-            <Link underline="always" sx={{ color: 'text.primary' }}>
+            <Link underline="always" sx={{ color: "text.primary" }}>
               Terms of Service
             </Link>
             &nbsp;and&nbsp;
-            <Link underline="always" sx={{ color: 'text.primary' }}>
+            <Link underline="always" sx={{ color: "text.primary" }}>
               Privacy Policy
             </Link>
             .
           </Typography>
 
           <MHidden width="smUp">
-            <Typography variant="subtitle2" sx={{ mt: 3, textAlign: 'center' }}>
+            <Typography variant="subtitle2" sx={{ mt: 3, textAlign: "center" }}>
               Sudah punya akun?&nbsp;
               <Link to="/login" component={RouterLink}>
                 Login
