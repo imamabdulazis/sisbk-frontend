99380161834a3290b73913821da73b3a13cd35cb
Imam <imam.azis@pgi-data.com>
2021-10-15 00:29:05 +0700

    update url production

--- src/api/apiHandler.js
+++ src/api/apiHandler.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: "https://sisbk-backend.herokuapp.com",
+  // baseURL: "http://localhost:4000",
+  baseURL: "https://sisbk-backend.herokuapp.com",
   headers: {
     Authorization: `Bearer ${localStorage.getItem("token")}`,
   },
