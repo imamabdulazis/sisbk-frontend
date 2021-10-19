421f0e88edf39a9d219a851be07e14478888682e
Imam <imam.azis@pgi-data.com>
2021-10-19 22:22:22 +0700

    update

--- src/components/chat/MainChat.js
+++ src/components/chat/MainChat.js
@@ -25,13 +25,13 @@ function MainChat(props) {
     <ChatEngineWrapper>
       <ChatSocket
         projectID="0535de57-3586-4aed-a156-edc6c4912ef6"
-        chatID="63354"
-        chatAccessKey="ca-6f0197df-d78a-4a88-ae89-f4ca493dc9c1"
+        chatID="64264"
+        chatAccessKey="ca-9cc50287-60d5-4b79-897e-69ada341b285"
         senderUsername={
           user?.previlage == "Guru" ? `${user?.name} - Guru` : user?.name
         }
       />
-      <ChatFeed activeChat={"63354"} />
+      <ChatFeed activeChat={"64264"} />
     </ChatEngineWrapper>
   );
 }
